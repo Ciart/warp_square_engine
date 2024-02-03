9223b3b0b3222cad54c30429db0c47970e6a8170
SoexDobin <a90189829@gmail.com>
2024-02-03 15:35:01 +0900

    기본 사용자 i/o 검증할 if match 문 작성

--- examples/sandbox.rs
+++ examples/sandbox.rs
@@ -2,19 +2,13 @@ extern crate warp_square_engine;
 
 use std::io;
 use warp_square_engine::{game::Game, piece_move::PieceMove, square::{File, Level, Rank, Square}};
-
-enum SelectableOption {
-
-}
+use warp_square_engine::board::{Board, BoardSnapshot};
+use warp_square_engine::piece::{Piece, PieceType};
+use warp_square_engine::square::Color;
 
 fn main() {
-
     show_readme();
     init_selection();
-    /*
-
-    / 15
-    */
 }
 fn show_readme() {
     let summary = " Summary ! ";
@@ -61,8 +55,10 @@ fn init_selection() {
     };
 }
 
-fn init_by_user_input() {
 
+const FEN_SLASH : usize = 15;
+const FEN_SIMPLIFY_SLASH : usize = 11;
+fn init_by_user_input() {
     println!("Using Fen method");
     println!("Start at up to down & left to right");
     println!("You dont have to write level Neutral, black, white! only q1~q6, k1~k6");
@@ -70,20 +66,156 @@ fn init_by_user_input() {
     println!("Like this : nbbn/p2p/4/4/4/4/4/4/4/4/P1P1/NBBN/q122/q622/k1pp/k6rr2");
     println!("If you dont need sub boards, you dont have to write but it will be empty");
     println!("Like this : nbbn/p2p/4/4/4/4/4/4/4/4/P1P1/NBBN");
+    print!("Please write init pos by FEN : ");
 
-    println!("Please write init pos by FEN");
+    loop {
+        let mut input = String::new();
+        let result = io::stdin()
+            .read_line(&mut input);
 
-    let mut input: String::new();
-    let result = std::io::stdin()
-        .read_line(&mut input);
+        let fen = match result {
+            Ok(_) => input.trim(),
+            Err(_) => {
+                eprintln!("Some Err write exactly");
+                std::process::exit(1);
+            }
+        };
 
-    let fen = match result {
-        Ok(_) => input.trim(),
-        Err(error) => {
-            eprintln!("Some Err write exactly");
-            std::process::exit(1);
+        let slash_count = fen.matches('/').count();
+        match slash_count {
+            FEN_SLASH => {
+                let is_valid_string = fen.is_fenric(FEN_SLASH);
+                let is_valid_string_count = match fen.chars().count() {
+                    43..=87 => true,
+                    _ => false,
+                };
+                // 1 ~ 11개 까지는 1234 // 12 ~ 15 는 12
+                // pP bB rR nN kK qQ 확인
+                if is_valid_string & is_valid_string_count {
+                    println!("15ok");
+
+                    break;
+                }
+
+            },
+            FEN_SIMPLIFY_SLASH => {
+                let is_valid_string = fen.is_fenric(FEN_SIMPLIFY_SLASH);
+                let is_valid_string_count = match fen.chars().count() {
+                    19..=55 => true,
+                    _ => false,
+                };
+
+                if is_valid_string & is_valid_string_count {
+                    println!("11ok");
+
+                    break;
+                }
+
+            }
+            _ => { println!("You write wrong!\n"); continue },
         }
-    };
-    // 잘못 쓴거 체크해서 루프 돌기
+    }
     // 4개일때 3개일때 2개일때 1개일때
+    // 벡터로 배열 만들어 자르고 숫자면은 배열 스킵 기물 알파벳이 있으면 init
+}
+
+pub trait CheckFen {
+    fn is_fenric(&self, slash_count : usize ) -> bool;
+}
+
+impl CheckFen for str {
+    fn is_fenric(&self, slash_count : usize ) -> bool {
+        let piece_str_vec = vec![
+            PieceType::Pawn.get_char(Color::White),
+            PieceType::Bishop.get_char(Color::White),
+            PieceType::Knight.get_char(Color::White),
+            PieceType::King.get_char(Color::White),
+            PieceType::Queen.get_char(Color::White),
+            PieceType::Rook.get_char(Color::White),
+
+            PieceType::Pawn.get_char(Color::Black),
+            PieceType::Bishop.get_char(Color::Black),
+            PieceType::Knight.get_char(Color::Black),
+            PieceType::King.get_char(Color::Black),
+            PieceType::Queen.get_char(Color::Black),
+            PieceType::Rook.get_char(Color::Black),
+        ];
+        let void_char_vec = vec!['1', '2', '3', '4'];
+        let void_char_vec_for_sub = vec!['1', '2'];
+
+        let void_str_sub_level_vec = vec![
+            "q1", "q2", "q3", "q4", "q5", "q6",
+            "k1", "k2", "k3", "k4", "k5", "k6",
+        ];
+        // todo 검사 필요
+
+
+        println!("{:?}", parts);
+
+        if slash_count == FEN_SIMPLIFY_SLASH {
+            let parts : Vec<&str> = self.split('/').collect();
+
+            for &part in parts.iter() {
+                if !(1..=4).contains(&part.chars().count()) {
+                    eprintln!("Err : You write overflow in fen! at ..{}..", part);
+                    return false;
+                }
+                if !piece_str_vec.iter().any(|&piece| part.contains(piece) || part.find(piece).is_none()) {
+                    eprintln!("Err : You dont write correct piece type in fen! at ..{}..", part);
+                    return false;
+                }
+                if !void_char_vec.iter().any(|&void_count| part.contains(void_count) || part.find(void_count).is_none()) {
+                    eprintln!("Err : You dont write correct void in fen! at ..{}..", part);
+                    return false;
+                }
+            }
+        }
+        else if slash_count == FEN_SLASH {
+            let main_parts : Vec<&str> = self.split('/').take(12).collect();
+            let sub_parts : Vec<&str> = self.split(('/')).skip(12).collect();
+
+            for &part in main_parts.iter() {
+                if !(1..=4).contains(&part.chars().count()) {
+                    eprintln!("Err : You write overflow in fen! at ..{}..", part);
+                    return false;
+                }
+                if !piece_str_vec.iter().any(|&piece| part.contains(piece) || part.find(piece).is_none()) {
+                    eprintln!("Err : You dont write correct piece type in fen! at ..{}..", part);
+                    return false;
+                }
+                if !void_char_vec.iter().any(|&void_count| part.contains(void_count) || part.find(void_count).is_none()) {
+                    eprintln!("Err : You dont write correct void in fen! at ..{}..", part);
+                    return false;
+                }
+            }
+            for &part in sub_parts.iter() {
+                if !(4..=6).contains(&part.chars().count()) {
+                    println!("?? {}", &part.chars().count());
+                    eprintln!("Err : You write overflow in fen! at ..{}..", part);
+                    return false;
+                }
+                if !void_str_sub_level_vec.iter().any(|&level| part[0..2].contains(level) ){
+                    eprintln!("Err : You dont write level or write wrong in fen! at ..{}..", part);
+                    return false;
+                }
+                if !piece_str_vec.iter().any(|&piece| part[2..].contains(piece) ||
+                    part[2..].find(piece).is_none()) {
+                    eprintln!("Err : You dont write correct piece type in fen! at ..{}..", part);
+                    return false;
+                }
+                if !void_char_vec_for_sub.iter().any(|&void_count| part[2..].contains(void_count) ||
+                    part[2..].find(void_count).is_none()) {
+                    eprintln!("Err : You dont write correct void in fen! at ..{}..", part);
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+}
+
+struct SandBoxMode {
+    pub board: Board,
+    pub move_stack: Vec<(PieceMove, BoardSnapshot)>,
 }
